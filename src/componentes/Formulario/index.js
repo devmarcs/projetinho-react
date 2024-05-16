dd81484751e8aa306695513a0e9b4cc4d9ac7c22
devmarcs <devmarcelo.gus@gmail.com>
2024-05-15 23:45:54 -0300

    Criação da lista suspensa

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,24 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 
 const Formulario = (props) => {
+
+
+
+    const times = [
+        'Ruby',
+        'JavaScript',
+        'Python',
+        'Go',
+        'Java',
+        'C',
+        'C++',
+        'Kotlin',
+        'Flutter'
+
+    ]
+
     return (
         <section className='formulario'>
             <form>
@@ -9,6 +26,7 @@ const Formulario = (props) => {
                 <CampoTexto label="Nome" placeholder="Digite o seu Nome" />
                 <CampoTexto label="Cargo" placeholder="Digite o seu Cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa label="Tecnologias:" itens={times}/>
             </form>
         </section>
     )
